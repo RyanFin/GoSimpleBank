f161dbfc4a650138e91ef79a6edbf9dd52c6fb9a
RyanFin <ryanfinlayson125@hotmail.com>
2024-04-02 23:09:37 +0100

    add unit test

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -1,6 +1,27 @@
 package db
 
-import "testing"
-func TestCreateAccount(t *testing.T){
+import (
+	"context"
+	"testing"
 
+	"github.com/stretchr/testify/assert"
+)
+func TestCreateAccount(t *testing.T){
+// create account params must be created
+args := CreateAccountParams{
+	Owner: "ryan",
+	Balance: 1500,
+	Currency: "GBP",
+}
+
+account, err := testQueries.CreateAccount(context.Background(), args)
+assert.NoError(t, err)
+assert.NotEmpty(t, account)
+
+assert.Equal(t, args.Owner, account.Owner)
+assert.Equal(t, args.Balance, account.Balance)
+assert.Equal(t, args.Currency, account.Currency)
+
+assert.NotZero(t, account.ID)
+assert.NotZero(t, account.CreatedAt)
 }
