088cc8371a1cbd8d68c2ddbcbd56d89e2fee2cb4
RyanFin <ryanfinlayson125@hotmail.com>
2024-06-23 01:26:26 +0100

    display both gRPC and HTTP metadata that can be populated from a gPRC client (Evans) or a HTTP request (Postman)

--- gapi/metadata.go
+++ gapi/metadata.go
@@ -5,11 +5,13 @@ import (
 	"log"
 
 	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/peer"
 )
 
 const (
 	grpcGatewayUserAgentHeader = "grpcgateway-user-agent"
 	xForwardForHeader          = "x-forwarded-for"
+	userAgentHeader            = "user-agent"
 )
 
 type Metadata struct {
@@ -22,14 +24,22 @@ func (server *Server) extractMetadata(ctx context.Context) *Metadata {
 	if md, ok := metadata.FromIncomingContext(ctx); ok {
 		log.Printf("md %v\n", md)
 
-		// check if userAgents slice is not empty
 		if userAgents := md.Get(grpcGatewayUserAgentHeader); len(userAgents) > 0 {
+			mtdt.UserAgent = userAgents[0]
+		}
+
+		// check if userAgents slice is not empty
+		if userAgents := md.Get(userAgentHeader); len(userAgents) > 0 {
 			mtdt.UserAgent = userAgents[0]
 		}
 
 		if clientIP := md.Get(xForwardForHeader); len(clientIP) > 0 {
 			mtdt.ClientIP = clientIP[0]
 		}
+	}
+
+	if p, ok := peer.FromContext(ctx); ok {
+		mtdt.ClientIP = p.Addr.String()
 	}
 
 	return mtdt
